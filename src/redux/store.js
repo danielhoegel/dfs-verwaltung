b6a4933acd9066d909db1b329f45d05f2c5cc7bf
Daniel Högel <hoegel.daniel@gmail.com>
2018-10-04 14:12:17 +0200

    Enable explicit hot reloading reducers
    More Infos: https://github.com/reduxjs/react-redux/releases/tag/v2.0.0

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,4 +7,12 @@ const middleware = applyMiddleware(logger, apiRequests);
 
 const store = createStore(reducer, middleware);
 
+if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('./rootReducer', () => {
+        const nextReducer = require('./rootReducer');
+        store.replaceReducer(nextReducer);
+    });
+}
+
 export default store;
