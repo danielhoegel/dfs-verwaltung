3555883b13b04a32b319022a0fe1f3bc1883fc09
Daniel Högel <hoegel.daniel@gmail.com>
2018-10-04 14:10:05 +0200

    Fix error with material-ui on hot reloading
    Error Message: "cannot read property refs of undefined"
    Solution: https://github.com/mui-org/material-ui/issues/9287#issuecomment-370228145

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import 'typeface-roboto';
 
-import store from './redux/store';
 import routes from './routes';
 import { FilterContextProvider } from './components/filter/FilterContext';
 import Layout from './components/layout/Layout';
 
-const theme = createMuiTheme({
-    palette: {
-        primary: { main: '#006ab3' },
-        secondary: { main: '#e2e3e3' },
-    }
-});
 
 class App extends Component {
     render() {
         return (
-            <Provider store={store} >
-                <Router>
-                    <FilterContextProvider>
-                        <MuiThemeProvider theme={theme}>
-                            <CssBaseline />
-                            <Layout>
-                                <Switch>
-                                    <Redirect from='/' to='/studenten' exact />
-                                    {routes.map(route => (
-                                        <Route {...route} key={route.path} />
-                                    ))}
-                                </Switch>
-                            </Layout>
-                        </MuiThemeProvider>
-                    </FilterContextProvider>
-                </Router>
-            </Provider>
+            <BrowserRouter>
+                <FilterContextProvider>
+                    <CssBaseline />
+                    <Layout>
+                        <Switch>
+                            <Redirect from='/' to='/studenten' exact />
+                            {routes.map(route => (
+                                <Route {...route} key={route.path} />
+                            ))}
+                        </Switch>
+                    </Layout>
+                </FilterContextProvider>
+            </BrowserRouter>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
+import theme from './theme';
+import store from './redux/store';
 import App from './App';
 
-const render = () => {
+const render = (AppComponent) => {
     ReactDOM.render(
-        <AppContainer>
-            <App />
-        </AppContainer>,
+        <MuiThemeProvider theme={theme}>
+            <AppContainer>
+                <Provider store={store}>
+                    <AppComponent />
+                </Provider>
+            </AppContainer>
+        </MuiThemeProvider>,
         document.getElementById('root')
     );
 };
 
 // Render once
-render();
+render(App);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
     module.hot.accept('./App', () => {
-        render();
+        const nextApp = require('./App').default;
+        render(nextApp);
     });
 }
+
+
+
