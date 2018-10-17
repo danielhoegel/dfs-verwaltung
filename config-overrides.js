670675543d948bb0ee8ad0515237b04ec10ad12a
Unknown <hoegel.daniel@gmail.com>
2018-10-18 01:16:57 +0200

    Update react-scripts to version 2
    - native SCCS support
    - use browserList for CSS

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,6 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const rewireSass = require('react-app-rewire-scss');
 
 module.exports = function override(config, env) {
   config = rewireReactHotLoader(config, env);
-  config = rewireSass(config, env);
   return config;
 }
