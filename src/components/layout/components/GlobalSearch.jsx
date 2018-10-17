fd84f221080905bf29599a48638068fe02e6d8c4
Unknown <hoegel.daniel@gmail.com>
2018-10-18 01:26:52 +0200

    Use custom search component for GlobalSearch + async search requests

--- src/components/layout/components/GlobalSearch.jsx
+++ src/components/layout/components/GlobalSearch.jsx
@@ -1,28 +1,131 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
-import Input from '@material-ui/core/Input';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import SearchIcon from '@material-ui/icons/Search';
+import Search from '../../Search';
 
-// import { stringsMatch } from '../../helper/helper';
+import { isNotEmpty } from '../../../helper/helper';
+import { intersectionBy } from 'lodash';
+
+
+const GROUPS = {
+    'SUBJECTS': 'Fächer',
+    'STUDENTS': 'Studenten'
+};
+
 
 class GlobalSearch extends Component {
+    state = {
+        searchValue: '',
+        fetching: false,
+        options: [],
+        subjects: [], // stored just for reference
+        students: [], // stored just for reference
+    }
+
+    changeHandler = (searchValue) => {
+        // console.log('CHANGE', { searchValue });
+        if (isNotEmpty(searchValue)) {
+            this.queryData(searchValue);
+            this.setState({ searchValue });
+        } else {
+            this.setState({
+                searchValue: '',
+                options: [],
+            })
+        }
+    }
+
+    selectHandler = (value, group) => {
+        console.log('SELECT', { value, group });
+        if (group === GROUPS.STUDENTS) {
+            this.props.history.push(`/studenten/${value}`);
+        } else if (group === GROUPS.SUBJECTS) {
+            console.log('SELECT SUBJECT - subjects:', this.state.subjects);
+            const { studyRegulationId } = this.state.subjects.filter(subject => subject.id === value)[0];
+            this.props.history.push(`/studienkurse/studienordnung/${studyRegulationId}/${value}`);
+        }
+    }
+
+    queryData(searchValue) {
+        // this.setState({ fetching: true });
+
+        // remove spaces at start and end, replace multiple spaces with single space
+        const trimmedSearchValue = searchValue.trim().replace(/ +(?= )/g,'');
+        
+        const studentRequests = [];
+        const searchWords = trimmedSearchValue.split(' ');
+        searchWords.forEach(searchWord => {
+            studentRequests.push(axios.get(`/students?q=${searchWord}&_limit=3`));
+        });
+
+        const subjectRequest = axios.get(`/subjects?_expand=studyCourse&_expand=studyRegulation&title_like=${trimmedSearchValue}&_limit=3`);
+
+        // combine all requests
+        axios.all(studentRequests.concat(subjectRequest))
+            .then(results => {
+                // remove the last element from results and save it in matchingSubjects
+                const matchingSubjects = results.splice(-1)[0].data;
+                
+                // create intersection of all student requests
+                const studentArrays = results.map(studentResult => studentResult.data);
+                const matchingStudents = intersectionBy(...studentArrays, 'id');
+                
+                const studentOptions = matchingStudents.map(student => ({
+                    label: `${student.firstName} ${student.lastName}`,
+                    value: student.id,
+                    secondary: `Matrikelnummer: ${student.matrikelnummer}`,
+                    group: GROUPS.STUDENTS,
+                }));
+
+                const subjectOptions = matchingSubjects.map(subject => ({
+                    label: `${subject.title}`,
+                    value: subject.id,
+                    secondary: `${subject.studyCourse.title}, ${subject.studyRegulation.title}`,
+                    group: GROUPS.SUBJECTS,
+                }));
+
+                this.setState({ 
+                    options: studentOptions.concat(subjectOptions),
+                    fetching: false,
+                    students: matchingStudents, // stored just for reference
+                    subjects: matchingSubjects, // stored just for reference
+                });
+            });
+    }
+
     render() {
         const { classes } = this.props;
+
         return (
             <div className={classes.search}>
                 <div className={classes.searchIcon}>
-                    <SearchIcon />
+                    {this.state.fetching
+                        ? <CircularProgress color='inherit' size={24} />
+                        : <SearchIcon />
+                    }
                 </div>
-                <Input
-                    placeholder="Suchen..."
-                    disableUnderline
-                    classes={{
-                        root: classes.inputRoot,
-                        input: classes.inputInput,
+
+                <Search
+                    searchValue={this.state.searchValue}
+                    options={this.state.options}
+                    onSelect={this.selectHandler}
+                    onInputChange={this.changeHandler}
+                    placeholder='Suchen...'
+                    popperAlign='right'
+                    maxHeight='320px'
+                    fixedWidth='420px'
+                    noClearIcon
+                    noDropdownIcon
+                    floatingMenu
+                    grouped
+                    inputProps={{
+                        disableUnderline: true,
+                        classes: { root: classes.inputRoot, input: classes.inputInput }
                     }}
                 />
           </div>
@@ -33,6 +136,8 @@ class GlobalSearch extends Component {
 
 const styles = theme => ({
     search: {
+        display: 'flex',
+        alignItems: 'center',
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
         backgroundColor: fade(theme.palette.common.white, 0.15),
@@ -47,32 +152,27 @@ const styles = theme => ({
         },
     },
     searchIcon: {
-        width: theme.spacing.unit * 9,
-        height: '100%',
-        position: 'absolute',
+        margin: [[0, 3 * theme.spacing.unit]],
         pointerEvents: 'none',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center',
     },
     inputRoot: {
         color: 'inherit',
-        width: '100%',
     },
     inputInput: {
-        paddingTop: theme.spacing.unit,
-        paddingRight: theme.spacing.unit,
-        paddingBottom: theme.spacing.unit,
-        paddingLeft: theme.spacing.unit * 10,
-        transition: theme.transitions.create('width'),
         width: '100%',
+        paddingRight: theme.spacing.unit,
         [theme.breakpoints.up('sm')]: {
             width: 120,
+            transition: theme.transitions.create('width'),
             '&:focus': {
-                 width: 200,
+                    width: 200,
             },
         },
     },
-  });
+});
 
-export default withStyles(styles)(GlobalSearch);
+export default withRouter(
+    withStyles(styles)(GlobalSearch)
+);
