2896b48f1bbdcd120290e25dcb86428bf455857e
Daniel Högel <hoegel.daniel@gmail.com>
2019-03-13 01:03:28 +0100

    Sort students by lastName and firstName

--- src/routes/studenten/redux/studentenSelectors.js
+++ src/routes/studenten/redux/studentenSelectors.js
@@ -20,35 +20,48 @@ export function getStudentenFilter(state) {
 
 /* Derived State */
 
+function __studentMatchesFilter(student, filter, searchString) {
+    if (isNotEmpty(student.studies)) {
+        const studyMatch = student.studies.some(study => {
+            if (filter.year.length && !filter.year.includes(study.year) ) {
+                return false;
+            }
+            if (isNotEmpty(filter.studyCourse) && filter.studyCourse !== study.studyCourseId) {
+                return false;
+            }
+            if (isNotEmpty(filter.status) && filter.status !== study.status) {
+                return false;
+            }
+            return true;
+        });
+        if (!studyMatch) return false;            
+    } else if (filter.studies || isNotEmpty(filter.studyCourse) || isNotEmpty(filter.status)) {
+        return false;
+    }
+
+    const studentMatch = (
+        !filter.student ||
+        student.firstName.toLocaleLowerCase().indexOf(searchString) !== -1 ||
+        student.lastName.toLocaleLowerCase().indexOf(searchString) !== -1 ||
+        student.matrikelnummer.indexOf(Number(searchString)) !== -1
+    );
+    return studentMatch;
+}
+
+function __sortStudents(studentA, studentB) {
+    /* localCompare returns 1, 0 or -1;
+     * 1 and -1 are truthy values
+     */
+    return (
+        studentA.lastName.localeCompare(studentB.lastName) ||
+        studentA.firstName.localeCompare(studentB.firstName)
+    );
+}
+
 export function getFilteredStudenten(state) {
     const studenten = getFullStudents(state);
     const { filter, searchString } = getStudentenData(state);
-
-    return studenten.filter(student => {
-        if (isNotEmpty(student.studies)) {
-            const studyMatch = student.studies.some(study => {
-                if (filter.year.length && !filter.year.includes(study.year) ) {
-                    return false;
-                }
-                if (isNotEmpty(filter.studyCourse) && filter.studyCourse !== study.studyCourseId) {
-                    return false;
-                }
-                if (isNotEmpty(filter.status) && filter.status !== study.status) {
-                    return false;
-                }
-                return true;
-            });
-            if (!studyMatch) return false;            
-        } else if (filter.studies || isNotEmpty(filter.studyCourse) || isNotEmpty(filter.status)) {
-            return false;
-        }
-
-        const studentMatch = (
-            !filter.student ||
-            student.firstName.toLocaleLowerCase().indexOf(searchString) !== -1 ||
-            student.lastName.toLocaleLowerCase().indexOf(searchString) !== -1 ||
-            student.matrikelnummer.indexOf(Number(searchString)) !== -1
-        );
-        return studentMatch;
-    });
+    return studenten
+        .filter(student => __studentMatchesFilter(student, filter, searchString))
+        .sort(__sortStudents);
 };
