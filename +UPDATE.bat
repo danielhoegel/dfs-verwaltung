b2918b4294fb8394b8401195ce621f4ab853bf06
Daniel Högel <hoegel.daniel@gmail.com>
2019-03-13 03:19:39 +0100

    Use local ssh key for update

--- +UPDATE.bat
+++ +UPDATE.bat
@@ -1,2 +1,3 @@
-git fetch --all && git reset --hard origin/master && npm install && npm run build
+git config core.sshCommand "ssh -i server/ssh_key -F /dev/null"
+git fetch --all && git reset --hard origin/master && npm install && npm run build && npm run production
 PAUSE
---
+++ server/frontend-server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const open = require('open');
+const path = require('path');
+
+const server = express();
+
+// static file declaration
+server.use(express.static(path.join(__dirname, '../build')));
+
+// always server index.html
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+const PORT = process.env.PORT || 3303;
+
+server.listen(PORT, () => {
+    console.log(`Frontend server startet on http://localhost:${PORT}`);
+    open(`http://localhost:${PORT}`);
+});
