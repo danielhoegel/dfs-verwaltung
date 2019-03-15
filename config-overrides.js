5adcf778b77cf60372e8fc8e96a09ba35927b08a
Daniel Högel <hoegel.daniel@gmail.com>
2019-03-15 13:31:33 +0100

    Replace react-dom with @hot-loader/react-dom

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,16 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 module.exports = function override(config, env) {
-  config = rewireReactHotLoader(config, env);
-  return config;
+    // react hot loader
+    config = rewireReactHotLoader(config, env);
+
+    // replace react-dom with @hot-loader/react-dom (https://github.com/gaearon/react-hot-loader#react--dom)
+    config.resolve = {
+        ...config.resolve,
+        alias: {
+            ...config.resolve.alias,
+            'react-dom': '@hot-loader/react-dom'
+        }
+    };
+    return config;
 }
