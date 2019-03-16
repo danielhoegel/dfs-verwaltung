501d6b9dc16e4e0c90063412afea4839914e3a08
Daniel Högel <hoegel.daniel@gmail.com>
2019-03-16 21:52:59 +0100

    Alow only 1 studyCourse in StudentCreate/Update to prevent server crash

--- src/components/fields/StudentCombinedFields.jsx
+++ src/components/fields/StudentCombinedFields.jsx
@@ -7,40 +7,38 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
-// import Field from '../Field';
-// import FieldGroup from '../FieldGroup';
 import HiddenDivider from '../HiddenDivider';
-import FieldArray from '../FieldArray';
 import StudentFields from './StudentFields';
 import StudentInformationFields from './StudentInformationFields';
 import StudyFields from './StudyFields';
 import MessageBox from '../MessageBox';
+// import FieldArray from '../FieldArray';
 
 
 const StudentCombinedFields = ({
     change,
     values,
     onCancel,
-    add,
-    remove,
+    // add,
+    // remove,
     studyRegulations,
     studyCourses,
     classes,
     history
 }) => {
-    const addStudyHandler = (index) => {
-        const item = {
-            year: new Date().getFullYear(),
-            studyCourseId: 1,
-            studyRegulationId: '',
-            status: 1,
-        };
-        add('studies', item);
-    }
+    // const addStudyHandler = (index) => {
+    //     const item = {
+    //         year: new Date().getFullYear(),
+    //         studyCourseId: 1,
+    //         studyRegulationId: '',
+    //         status: 1,
+    //     };
+    //     add('studies', item);
+    // }
 
-    const removeStudyHandler = (index) => {
-        remove('studies', index);
-    }
+    // const removeStudyHandler = (index) => {
+    //     remove('studies', index);
+    // }
 
     // check missing data
     if (!studyRegulations.length || !studyCourses.length) {
@@ -66,11 +64,13 @@ const StudentCombinedFields = ({
                 <StudentFields
                     onChange={change}
                     values={values.student}
+                    prefix='student'
                 />
 
                 <StudentInformationFields
                     onChange={change}
                     values={values.studentInformation}
+                    prefix='studentInformation'
                 />
             </Paper>
 
@@ -78,7 +78,14 @@ const StudentCombinedFields = ({
             <Typography component='h3' variant='h6'>
                 Studium
             </Typography>
-            <FieldArray
+            <StudyFields
+                change={change}
+                values={values.studies[0]}
+                prefix='studies[0]'
+                studyRegulations={studyRegulations}
+                studyCourses={studyCourses}
+            />
+            {/* <FieldArray
                 component={StudyFields}
                 values={values.studies}
                 onChange={change}
@@ -90,7 +97,7 @@ const StudentCombinedFields = ({
                 min={1}
                 studyRegulations={studyRegulations}
                 studyCourses={studyCourses}
-            />
+            /> */}
 
             <HiddenDivider height={2} />
             <Button variant='contained' color='primary' type='submit'>
@@ -107,8 +114,8 @@ StudentCombinedFields.propTypes = {
     change: PropTypes.func.isRequired,
     values: PropTypes.object.isRequired,
     onCancel: PropTypes.func.isRequired,
-    add: PropTypes.func.isRequired,
-    remove: PropTypes.func.isRequired,
+    // add: PropTypes.func.isRequired,
+    // remove: PropTypes.func.isRequired,
     studyRegulations: PropTypes.array.isRequired,
     studyCourses: PropTypes.array.isRequired,
     classes: PropTypes.object.isRequired,
