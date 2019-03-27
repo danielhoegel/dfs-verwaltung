50b4cbcb8d112fe7789aae22c92b1996cf982faf
Daniel Högel <hoegel.daniel@gmail.com>
2019-03-27 20:04:13 +0100

    Update readme, gitignore nad update batch to new server

--- +UPDATE.bat
+++ +UPDATE.bat
@@ -1,1 +1,1 @@
-git config core.sshCommand "ssh -i datenbank/ssh_key -F /dev/null" && git fetch --all && git reset --hard origin/master && npm install && npm run build && npm run production
+git config core.sshCommand "ssh -i server/ssh_key -F /dev/null" && git fetch --all && git reset --hard origin/master && npm install && npm run build && npm run production
