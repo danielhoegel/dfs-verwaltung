192681e923609855998ea0aa5261fd9508f4ac46
Daniel Högel <hoegel.daniel@gmail.com>
2019-03-28 01:23:00 +0100

    Update export keys

--- src/helper/exportToCSV.js
+++ src/helper/exportToCSV.js
@@ -1,4 +1,4 @@
-import { isNotEmpty } from "./helper";
+import { isNotEmpty } from './helper';
 
 /**
  * source: https://stackoverflow.com/a/38021207/8936417
@@ -7,12 +7,12 @@ function downloadCSV(csvContent, fileName) {
     const encodedURI = encodeURI(csvContent);
     const link = document.createElement('a');
 
-    link.setAttribute('href', 'data:text/csv;charset=UTF-8,%EF%BB%BF' + encodedURI);
+    link.setAttribute('href', `data:text/csv;charset=UTF-8,%EF%BB%BF${encodedURI}`);
     link.setAttribute('download', `${fileName}_${Date.now()}.csv`);
-    
+
     document.body.appendChild(link); // Required for FF
     link.click();
-    document.body.removeChild(link); 
+    document.body.removeChild(link);
 }
 
 function arrayToCSVRow(dataArray) {
@@ -21,12 +21,19 @@ function arrayToCSVRow(dataArray) {
 
 export default function exportToCSV(arrayOfObjects, fileName = 'Export') {
     if (!arrayOfObjects.length) {
+        // eslint-disable-next-line no-alert
         alert('Keine Einträge vorhanden, die exportiert werden könnten.');
         return;
-    };
+    }
 
     const csvRows = [];
-    const keys = ['id', 'matrikelnummer', 'firstName', 'lastName', 'prefix', 'birthDate', 'birthPlace', 'birthCountry', 'street', 'streetNumber', 'addressExtra', 'postal', 'city', 'country', 'mailPrimary', 'mailSecondary', 'phoneNumber', 'mobileNumber'];
+    const keys = [
+        'id', 'matrikelnummer', 'firstName', 'lastName', 'prefix',
+        'birthDate', 'birthPlace', 'birthCountry',
+        'address', 'addressExtra', 'postal', 'city', 'country',
+        'mailPrimary', 'mailSecondary', 'mobileNumber', 'phoneNumber',
+        'iban', 'bic', 'bank', 'accountHolder'
+];
 
     // header
     csvRows.push(arrayToCSVRow(keys));
@@ -35,7 +42,7 @@ export default function exportToCSV(arrayOfObjects, fileName = 'Export') {
     arrayOfObjects.forEach(item => {
         const nextRow = keys.map(key => isNotEmpty(item[key]) ? item[key] : '');
         csvRows.push(arrayToCSVRow(nextRow));
-    }); 
+    });
 
     downloadCSV(csvRows.join('\n'), fileName);
 }
