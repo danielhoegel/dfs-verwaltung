76fd2748986a374d8d041ebabab44761c386e577
Daniel Högel <hoegel.daniel@gmail.com>
2019-09-27 15:44:25 +0200

    Make mor student infromation values copyable and style links

--- src/routes/studenten/studentenDetails/components/StudentInformation.jsx
+++ src/routes/studenten/studentenDetails/components/StudentInformation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import FileCopyIcon from '@material-ui/icons/FileCopyOutlined';
 
@@ -30,7 +30,10 @@ const StudentInformation = ({ classes, student }) => {
 
     const copyIban = () => copyToClipboard(iban);
     const copyBic = () => copyToClipboard(bic);
-
+    const copyMailPrimary = () => copyToClipboard(mailPrimary);
+    const copyMailSecondary = () => copyToClipboard(mailSecondary);
+    const copyPhoneNumber = () => copyToClipboard(phoneNumber);
+    const copyMobileNumber = () => copyToClipboard(mobileNumber);
 
     return (
         <div>
@@ -43,31 +46,42 @@ const StudentInformation = ({ classes, student }) => {
                     <span className={classes.noSelect}>Geburtsort: </span>
                     {birthPlace}{birthPlace && birthCountry && ', '}{birthCountry}
                 </div>
-            
+
             <Typography component='h3' variant='body1' className={classes.subheading}>Adresse</Typography>
                 {address && address}{address && addressExtra && ', '}{addressExtra}{address && <br />}
                 {postal}{postal && city && ' '}{city}{(postal || city) && country && ', '}{country}
 
             <Typography component='h3' variant='body1' className={classes.subheading}>E-Mail</Typography>
-            <div>
-                <span className={classes.noSelect}>Mail (Uni): </span>
-                <a href={`mailto:${mailPrimary}`} className={classes.link}>{mailPrimary}</a>
+            <div className={classes.copyContainer}>
+                <span>
+                    <span className={classes.noSelect}>Mail (Uni): </span>
+                    <a href={`mailto:${mailPrimary}`} className={classes.link}>{mailPrimary}</a>
+                </span>
+                {mailPrimary && <FileCopyIcon className={classes.copyIcon} onClick={copyMailPrimary} />}
             </div>
-            <div>
-                <span className={classes.noSelect}>Mail (Privat): </span>
-                <a href={`mailto:${mailSecondary}`} className={classes.link}>{mailSecondary}</a>
+            <div className={classes.copyContainer}>
+                <span>
+                    <span className={classes.noSelect}>Mail (Privat): </span>
+                    <a href={`mailto:${mailSecondary}`} className={classes.link}>{mailSecondary}</a>
+                </span>
+                {mailSecondary && <FileCopyIcon className={classes.copyIcon} onClick={copyMailSecondary} />}
             </div>
-                
+
             <Typography component='h3' variant='body1' className={classes.subheading}>Telefonnummer</Typography>
-            <div>
-                <span className={classes.noSelect}>Festnetz: </span>
-                <a href={`tel:${phoneNumber}`} className={classes.link}>{phoneNumber}</a>
+            <div className={classes.copyContainer}>
+                <span>
+                    <span className={classes.noSelect}>Festnetz: </span>
+                    <a href={`tel:${phoneNumber}`} className={classes.link}>{phoneNumber}</a>
+                </span>
+                {phoneNumber && <FileCopyIcon className={classes.copyIcon} onClick={copyPhoneNumber} />}
             </div>
-            <div>
-                <span className={classes.noSelect}>Mobieltelefon: </span>
-                <a href={`tel:${mobileNumber}`} className={classes.link}>{mobileNumber}</a>
+            <div className={classes.copyContainer}>
+                <span>
+                    <span className={classes.noSelect}>Mobieltelefon: </span>
+                    <a href={`tel:${mobileNumber}`} className={classes.link}>{mobileNumber}</a>
+                </span>
+                {mobileNumber && <FileCopyIcon className={classes.copyIcon} onClick={copyMobileNumber} />}
             </div>
-            
             <Typography component='h3' variant='body1' className={classes.subheading}>Kontodaten</Typography>
             <div>
                 <span>
@@ -114,11 +128,9 @@ const styles = theme => ({
     link: {
         color: theme.palette.primary.dark,
         textDecoration: 'none',
-        display: 'inline-block',
-        borderBottom: '1px dashed transparent',
         '&:hover': {
             color: theme.palette.primary.light,
-            borderColor: theme.palette.primary.light
+            textDecoration: 'underline'
         }
     },
     noSelect: {
@@ -144,6 +156,6 @@ const styles = theme => ({
             color: theme.palette.green,
         }
     },
-})
+});
 
 export default withStyles(styles)(StudentInformation);
